0b47a7e9df59ea41e1772bd1bfd10842c79e62c9
Cossegosse <cossegosse@gmail.com>
2023-08-21 10:22:52 +0200

    Update Distance.py

--- Distance.py
+++ Distance.py
@@ -4,13 +4,13 @@ Bensinpris = 20
 förbrukning = 1.5
 
 #hur långt åker du?
-distans = input("Hur långt ska du åka?")
+distans = float(input("Hur långt ska du åka?"))
 
 #räkna ut hur mycket bensin du använde under resan.
-
+användning = distans * förbrukning
 
 #räkna ut hur mycket resan kostar.
-
+kostnad = distans * användning
 
 #printa ut svaren.
-print(type(input_1))
+print("Du åkte ", distans, "km"," förbrukade ", användning, "L"," Det kostade ", kostnad, "kr")
