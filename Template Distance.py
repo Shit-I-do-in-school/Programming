cd8761b9abe84e24bd79ce987f10fd49b3f9f2fa
Cossegosse <94567782+Cossegosse@users.noreply.github.com>
2023-08-21 10:32:44 +0200

    Update and rename Distance.py to Template Distance.py

---
+++ Template Distance.py
@@ -0,0 +1,16 @@
+#bensin pris
+Bensinpris = 20
+#förbrukning per km.
+förbrukning = 1.5
+
+#hur långt åker du?
+
+
+#räkna ut hur mycket bensin du använde under resan.
+
+
+#räkna ut hur mycket resan kostar.
+
+
+#printa ut svaren.
+print("Du åkte ", Name, "km"," förbrukade ", Name, "L"," Det kostade ", Name, "kr")
