74779f488b7e4b5ed62a87f8c2afafc437b4b8a9
Cossegosse <cossegosse@gmail.com>
2023-08-21 09:39:56 +0200

    Template
    Easy to use template för upgift 1

---
+++ Distance.py
@@ -0,0 +1,16 @@
+#bensin pris
+Bensinpris = 20
+#förbrukning per km.
+förbrukning = 1.5
+
+#hur långt åker du?
+distans = input("Hur långt ska du åka?")
+
+#räkna ut hur mycket bensin du använde under resan.
+
+
+#räkna ut hur mycket resan kostar.
+
+
+#printa ut svaren.
+print(type(input_1))
